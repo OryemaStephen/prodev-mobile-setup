2502beb9f9f7ddf2610aab66461f76d1fd2edeb0
OryemaStephen <oryemasteve@gmail.com>
2025-08-25 16:35:32 +0300

    Updated gitignore

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { HapticTab } from "@/app-example/components/HapticTab";
+import { IconSymbol } from "@/app-example/components/ui/IconSymbol.ios";
+import BlurTabBarBackground from "@/app-example/components/ui/TabBarBackground.ios";
+import { Colors } from "@/app-example/constants/Colors";
+import { useColorScheme } from "@/app-example/hooks/useColorScheme.web";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: BlurTabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
